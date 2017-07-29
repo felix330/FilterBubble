93cdd92606c5b46e79d4f8df048ad32b76df9549
Felix <felix330@gmail.com>
2017-07-29 11:00:41 +0200

    Multiplayer tests

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMovement : NetworkBehaviour {
+
+	public GameObject child;
+	public GameObject objects;
+
+	// Use this for initialization
+	void Start () {
+		objects = GameObject.Find ("Objects");
+	}
+
+	public override void OnStartLocalPlayer() {
+		child.GetComponent<SpriteRenderer>().material.color = Color.green;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton ("Jump")) {
+			objects.transform.position = new Vector2 (0, 1);
+			objects.BroadcastMessage ("ReceiveMessage");
+		}
+
+		if (!isLocalPlayer)
+			return;
+
+
+
+		if (Input.GetAxis ("Horizontal") != 0) {
+			transform.position = new Vector2 (transform.position.x + Input.GetAxis ("Horizontal"), transform.position.y);
+		}
+	}
+}
