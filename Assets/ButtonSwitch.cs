dd0797fdcb0b091492f5df0270d4ead121d30264
Dominik Weis <d+github@w315.de>
2017-07-30 11:42:00 +0200

    Buttons + Folders

--- Assets/ButtonSwitch.cs
+++ Assets/ButtonSwitch.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.Networking;
 
 public class ButtonSwitch : NetworkBehaviour {
+	public GameObject trigger;
 
 	// Use this for initialization
 	void Start () {
@@ -13,5 +14,11 @@ public class ButtonSwitch : NetworkBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+	void OnTriggerEnter () {
+		//transform.Translate (Vector3.down);
+		transform.localPosition = new Vector3 (0,-0.3F,0.1F);
+		trigger.transform.Translate(new Vector3 (0, -3, 0));
+		//trigger.transform.transl
 	}
 }
